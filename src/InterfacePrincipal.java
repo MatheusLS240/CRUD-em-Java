deefd25e9130f2ed06e9962c7a65bba25200d06c
Matheuzinho <grandboss1978@gmail.com>
2025-06-08 19:02:54 -0300

    Base concluida, precisei de ajudan da IA para trabalhar com os indíces

--- src/InterfacePrincipal.java
+++ src/InterfacePrincipal.java
@@ -11,8 +11,9 @@ public class InterfacePrincipal {
     private JPanel campos;
     private JTextField campoTarefa;
     private JButton adicionarTarefaButton;
+    private boolean modoEdicao;
 
-    private ArrayList<JLabel> listaTarefas = new ArrayList<>();
+    private final ArrayList<JLabel> listaTarefas = new ArrayList<>();
 
     public InterfacePrincipal() {
         campos.setLayout(new BoxLayout(campos, BoxLayout.Y_AXIS));
@@ -36,7 +37,6 @@ public class InterfacePrincipal {
                     labelTarefa.setText(indice + " - " + textoDigitado);
 
                     JButton botaoRemover = new JButton("Remover");
-
                     botaoRemover.addActionListener(new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
@@ -44,20 +44,63 @@ public class InterfacePrincipal {
                             campos.revalidate();
                             campos.repaint();
                             listaTarefas.remove(labelTarefa);
+                            atualizarIndices();
+                        }
+                    });
+
+                    JButton editarLabel = new JButton("Editar");
+                    JLabel editar = new JLabel("Para editar, digite no campo de adicionar tarefa!");
+                    editar.setAlignmentX(Component.CENTER_ALIGNMENT);
+                    editar.setVisible(false);
+
+                    campos.add(editar);
+
+                    editarLabel.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            modoEdicao = !modoEdicao;
+
+                            adicionarTarefaButton.setVisible(!modoEdicao);
+                            botaoRemover.setVisible(!modoEdicao);
+                            editar.setVisible(modoEdicao);
+
+                            if (modoEdicao) {
+                                editarLabel.setText("Enviar Edição");
+                            } else {
+                                editarLabel.setText("Editar");
+                                String novoTexto = campoTarefa.getText().trim();
+
+                                if (!novoTexto.isEmpty()) {
+                                    int indiceAtual = listaTarefas.indexOf(labelTarefa) + 1;
+                                    labelTarefa.setText(indiceAtual + " - " + novoTexto);
+                                }
+                            }
+                            campoTarefa.setText("");
                         }
                     });
 
                     painelTarefa.add(labelTarefa);
                     painelTarefa.add(botaoRemover);
+                    painelTarefa.add(editarLabel);
 
                     campos.add(painelTarefa);
                     campoTarefa.setText("");
-
                     campos.revalidate();
                     campos.repaint();
                 }
             }
         });
+    }
+
+    private void atualizarIndices() {
+        for (int i = 0; i < listaTarefas.size(); i++) {
+            JLabel label = listaTarefas.get(i);
+            String textoAtual = label.getText();
+            String[] partes = textoAtual.split(" - ", 2);
+            if (partes.length == 2) {
+                label.setText((i + 1) + " - " + partes[1]);
+            }
+        }
     }
 
     public static void main(String[] args) {
